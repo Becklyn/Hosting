f25a3a2ff5021a77eaa53383dadf2a7d7a7392ca
Kai Eichinger <ke@becklyn.com>
2019-01-30 17:14:26 +0100

    Don’t explicitly check against BinaryFileResponse but only make sure the content is a string

--- src/Listener/MonitoringTokenListener.php
+++ src/Listener/MonitoringTokenListener.php
@@ -4,7 +4,6 @@ namespace Becklyn\Hosting\Listener;
 
 use Becklyn\Hosting\Config\HostingConfig;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -40,14 +39,14 @@ class MonitoringTokenListener implements EventSubscriberInterface
         $response = $event->getResponse();
 
         // skip if not HTML response
-        if ($response instanceof BinaryFileResponse || false === \strpos($response->headers->get("Content-Type"), "text/html"))
+        if (false === \strpos($response->headers->get("Content-Type"), "text/html"))
         {
             return;
         }
 
         $content = $response->getContent();
 
-        if (false !== ($position = \strrpos($content, '</body>')))
+        if (\is_string($content) && false !== ($position = \strrpos($content, '</body>')))
         {
             $content = \substr($content, 0, $position)
                 . $this->uptimeHtmlEmbed
