f381f1c7860b85224ed53cd1c5d307699e85160b
Manuel Goischke <mg@becklyn.com>
2019-11-20 18:27:06 +0100

    add test for GitIntegration

---
+++ tests/Git/GitIntegrationTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use Becklyn\Hosting\Git\GitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class GitIntegrationTest extends TestCase
+{
+    public function testFetchHeadCommitHash () : void
+    {
+        $gitIntegration = new GitIntegration(\implode("/", \array_slice(\explode("/", __DIR__), 0, -2)));
+
+        $this->assertNotNull($gitIntegration->fetchHeadCommitHash());
+    }
+}
